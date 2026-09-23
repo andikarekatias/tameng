14a0118c30ec75309b1416e98b2d3d13e10d7d2d
(andikarekatias <andika.rekatias@yahoo.com>
2026-09-23 15:07:54 +0700

    Fix CI on Laravel 12 prefer-lowest; set fail-fast false

--- tests/Fixtures/User.php
+++ tests/Fixtures/User.php
@@ -2,12 +2,12 @@
 
 namespace Andika\Tameng\Tests\Fixtures;
 
-use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable(['name', 'email', 'password'])]
 class User extends Authenticatable
 {
     use HasRoles;
+
+    protected $fillable = ['name', 'email', 'password'];
 }
